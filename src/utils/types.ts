f85806b86e14ebeb4bb2476376f9b6edc174625d
chikkibum <jackiechrs10@gmail.com>
2024-06-28 00:32:15 +0530

    project initiation

---
+++ src/utils/types.ts
@@ -0,0 +1,11 @@
+export type AlgorithmType = "DIJKSTRA" | "A_STAR" | "BFS" | "DFS";
+export interface AlgorithmSelectType {
+  name: string;
+  value: AlgorithmType;
+}
+
+export type MazeType = "NONE" | "BINARY_TREE" | "RECURSIVE_DIVISION";
+export interface MazeSelectType {
+  name: string;
+  value: MazeType;
+}
